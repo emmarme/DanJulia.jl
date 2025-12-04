5b7ec8a2e51d7165ec0e1456f7928b3ae3d1187d
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2025-12-04 10:45:17 +0100

    mise en page 2

--- exemple/try.jl
+++ exemple/try.jl
@@ -2,6 +2,24 @@ using GLMakie
 using LinearAlgebra
 include("../src/DanJulia.jl")
 using .DanJulia
+
+using Random,LinearAlgebra
+Random.seed!(2)
+n = 10
+m = 20
+sigma = 0.1
+X = randn(m,n)
+theta_true = rand(n)
+xi = randn(m)
+y = X * theta_true + sigma * xi
+
+DanJulia.p1(X, y)
+println("theta_vraie = ", theta_true)
+
+# pour S=3
+DanJulia.p2(X, y, 3.0)
+println("theta_vraie = ", theta_true)
+
 errors = []
 S_values = 1.0:1.0:10.0
 for S in S_values
@@ -13,4 +31,7 @@ end
 fig = Figure()
 ax = Axis(fig[1, 1], xlabel="S", ylabel="Erreur (norme 1)", title="Erreur entre theta_estime et theta_vraie en fonction de S")
 lines!(ax, S_values, errors)
-fig
+display(fig)
+
+#trouver le S optimal
+DanJulia.S_optimal(X,y,theta_true)
